15e33eb289253931094cc2965cb07cfc829b7272
aidan coyne <raptros.v76@gmail.com>
2014-11-26 14:16:27 -0600

    add rudimentary api client

---
+++ client/Chatless/Client.hs
@@ -0,0 +1,58 @@
+{-|
+Description: api client for chatless
+
+api client library for chatless.
+-}
+
+module Chatless.Client where
+
+import Control.Applicative
+import Network.HTTP.Client
+import Network.HTTP.Types.Method
+import Network.HTTP.Types.Header
+import Control.Lens
+import Data.Aeson
+import Chatless.Model.User
+import Chatless.Model.Topic
+import Control.Monad.IO.Class
+import Data.Default as Def
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
+import Data.Monoid
+
+data ApiBase = ApiBase {
+    apiHost :: BS.ByteString,
+    apiPort :: Int
+} deriving (Eq, Show)
+
+data Session = Session {
+    sessionUser :: BS.ByteString,
+    sessionApi :: ApiBase,
+    sessionManager :: Manager
+}
+
+mkAbsPath :: [BS.ByteString] -> BS.ByteString
+mkAbsPath pth = mconcat $ fmap ("/" <>) pth
+
+mkCLRequest :: MonadIO m => Session -> [BS.ByteString] -> StdMethod -> RequestHeaders -> RequestBody -> m (Response BSL.ByteString)
+mkCLRequest ses pth mth hdr bdy = liftIO $ httpLbs req (sessionManager ses)
+    where
+    req :: Request
+    req = Def.def { 
+        method = renderStdMethod mth,
+        host = apiHost . sessionApi $ ses,
+        port = apiPort . sessionApi $ ses,
+        path = mkAbsPath pth,
+        requestHeaders = ("x-chatless-test-uid", sessionUser ses):hdr,
+        requestBody = bdy
+    }
+
+mkCLQuery :: MonadIO m => Session -> [BS.ByteString] -> m (Response BSL.ByteString)
+mkCLQuery ses pth =  mkCLRequest ses pth GET [] (RequestBodyBS "")
+
+queryMe :: (Functor m, MonadIO m) => Session -> m (Response (Either String User))
+queryMe ses = fmap (eitherDecode' <$>) $ mkCLQuery ses ["me"]
+
+
+
